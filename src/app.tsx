ee17f248da7bb4add8af0ab0020d70316b6ced13
pamela vieira da silva <panhavsilva@gmail.com>
2021-09-19 16:40:06 -0300

    excluido estado fixo de file

--- src/app.tsx
+++ src/app.tsx
@@ -7,46 +7,7 @@ import { Content } from 'content'
 
 type FileArray = File[]
 export function App () {
-  const fileList: FileArray = [
-    {
-      id: v4(),
-      name: 'Readme.md',
-      content: `
-        <h1> Text h1</h1>
-        ## Text h2 <br />
-        ### Text h3 <br />
-        #### Text h4 <br />
-        ##### Text h5 <br />
-        ###### Text h6 <br />
-        Lorem ipsum dolor sit amet simet
-      `,
-      active: false,
-      status: 'editing',
-    },
-    {
-      id: v4(),
-      name: 'Contributing.md',
-      content: '## Text 2',
-      active: false,
-      status: 'saving',
-    },
-    {
-      id: v4(),
-      name: 'License.md',
-      content: '### Text 3',
-      active: false,
-      status: 'saved',
-    },
-    {
-      id: v4(),
-      name: 'Links.md',
-      content: '#### Text 4',
-      active: false,
-      status: 'saved',
-    },
-  ]
-
-  const [files, setFile] = useState<FileArray>(fileList)
+  const [files, setFile] = useState<FileArray>([])
 
   const handleClick = () => {
     const newFile: FileArray = [{
